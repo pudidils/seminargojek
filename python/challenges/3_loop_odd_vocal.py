34c06312b34afa25474a2cf364fa202aeb4a0512
pudidils <setsindonesia@gmail.com>
2025-10-29 11:29:24 +0700

    challenge3

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,11 +1,11 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
-odd_numbers = []
+odd_numbers = [x for x in range(1, 16) if x % 2 == 1]
 print(odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
 word = input("Masukkan kata: ").lower()
 
 vowels = "aiueo"
-count = 0
+count = sum(1 for x in word if x in vowels)
 
 print("Jumlah huruf vokal:", count)
--- python/challenges/4_function_is_even_factorial.py
+++ python/challenges/4_function_is_even_factorial.py
@@ -1,6 +1,8 @@
 # TODO: Fungsi untuk memeriksa apakah sebuah bilangan genap
 def is_even(num):
-    pass
+    if num % 2 == 0:
+        return True
+    return False
 
 # Tes fungsi
 print(is_even(4))   # True
@@ -8,7 +10,10 @@ print(is_even(7))   # False
 
 # TODO: Fungsi untuk menghitung faktorial
 def factorial(n):
-    pass
+    count = 1
+    for x in range(1, n+1):
+        count *= x
+    return count
 
 # Tes fungsi
 print(factorial(5))  # 120
