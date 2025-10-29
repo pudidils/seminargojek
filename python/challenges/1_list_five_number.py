0743c335b74f22ab31cf329d139587f56e001d8e
pudidils <setsindonesia@gmail.com>
2025-10-29 10:46:55 +0700

    solve challenge1

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -1,15 +1,18 @@
 # 1. Buat list berisi 5 angka
 numbers = [10, 25, 7, 40, 15]
-print("List awal:", numbers)
+print("List awal: ", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
-
+print("Angka pertama: ", end="")
+print(numbers[0])
+print("Angka terakhir: ", end="")
+print(numbers[-1])
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
-print("List setelah ditambah:", numbers)
+numbers.append(60)
+print("List setelah ditambah: ", numbers)
 
 # 4. TODO: Urutkan list secara descending
 
 # 5. Tampilkan hasil akhir
-print("List urut descending:", numbers)
+numbers.sort(reverse= True)
+print("List urut descending:",numbers,"coba")
